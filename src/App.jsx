7b7250452a7df9930de399f076078c26f425eb9d
panchamipammi123-maker <panchamipammi123@gmail.com>
2026-04-10 00:52:18 +0530

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import AboutUs from "./components/AboutUs";
+import ProductList from "./components/ProductList";
+import CartItem from "./components/CartItem";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Paradise Nursery</h1>
+      <p>Welcome to Paradise Nursery, your online plant store.</p>
+      <button>Get Started</button>
+    </div>
+  );
+}
+
+export default App;
